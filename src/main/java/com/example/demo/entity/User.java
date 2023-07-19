c9c130ea4bb22fbcf773a8ba26a67bbff930f765
jimmy2991 <jloh931@gmail.com>
2023-07-19 15:31:41 +0800

    connected to database

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,7 +11,7 @@ import jakarta.persistence.Table;
 public class User {
     @Column(name ="name")
     private String name;
-
+    @Id
     @Column(name ="staff_id")
     private String id;
 
